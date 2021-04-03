9a1b78af307541698c7bbd4a8c854c2a5133cf47
Alexander Dedykhin <vyshky2@gmail.com>
2021-04-03 20:27:21 +0400

    Operators overloading

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 #define tab '\t'
 
+//#define STRUCT
+//#define CONSTRUCKTOR_CHECK
+//#define ASSIGNMENT_CHECK
+//#define STREAMS
+
 class Point //Тип данных Point
 {
 	double x;
@@ -19,7 +24,6 @@ public:
 	{
 		this->x = x;
 	}
-
 	void set_y(double y)
 	{
 		this->y = y;
@@ -51,32 +55,97 @@ public:
 	}
 	~Point()
 	{
-		cout << "Destructor:\t" << this << endl;
+		cout << "Destructor:\t\t" << this << endl;
 	}
-	//Operators
-	void operator=(const Point& other)
+	//Copy Operators    (=) ,A = B
+	Point& /*предаем по ссылке чтобы  лишный раз не копировался объект */ operator = (const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssigment:\t\t" << this << endl;
+		return *this; // Передаем по значению
+	}
+	Point& operator+=(const Point& other)
+	{
+		/*this->x += other.x;
+		this->y += other.x;*/
+		this->set_x(this->x + other.x);
+		this->set_y(this->y + other.y);
+		return *this;
+	}
+	// Increment/Decrement
+	Point& operator++() //Prefix increment
+	{
+		++this->x;
+		++this->y;
+		return *this;
+	}
+	Point operator++(int) //Postfix inrement
+	{
+		Point old = *this;
+		/*this->x++;
+		this->y++;*/
+		++* this; //вызвали префиксный инкремент Prefix increment
+		return old;
 	}
 
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
+	}
 	//Methods
 	void print()const
 	{
 		cout << "X = " << x << tab << "Y = " << y << endl;
 	}
-
-
-
 };
 //Cтруктура - это тип данных , Сlass - это тип данных
 
-//#define STRUCT
 
+
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+	{
+		return true;
+	}
+	else
+		return false;*/
+
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+
+}
+bool operator!=(const Point& left, const Point& right)
+{
+	return !(left == right);
+}
+ostream& operator<<(ostream& os, Point& obj)
+{
+	os << "X = " << obj.get_x() << tab << "Y = " << obj.get_y();
+	return os;
+}
+
+istream& operator>>(istream& is, Point& obj)
+{
+	double x, y;
+	is >> x >> y;
+	obj.set_x(x);
+	obj.set_y(y);
+	return is;
+}
 void main()
 {
-	setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "Russian");
 #ifdef STRUCT
 
 	int a;     // Объявляем переменную а типа int 
@@ -93,10 +162,11 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STUCT
 
+#ifdef CONSTRUCKTOR_CHECK
 	Point A; //Объявляем объект класса Поинт/defualt constructor
-	/*A.set_x(2);
-	A.set_y(3);*/
-	//cout << A.get_x() << tab << A.get_y() << endl;
+/*A.set_x(2);
+A.set_y(3);*/
+//cout << A.get_x() << tab << A.get_y() << endl;
 	A.print();
 	Point B(2, 3);
 	B.print();
@@ -104,10 +174,61 @@ void main()
 	Point C = 5;//Single - argument constructer
 	C.print();
 
-	Point D = B; //Copy Constructor
+	Point D = B; //Copy Constructor	
 	D.print();
 
-	Point E; //Default constructor
-	E = D;   //Copy Assignment
+	Point E; //Default constructor 	
+	E = D;   //Copy Assignment	Перед присваиваем отчищает память.
 	E.print();
+#endif // CONSTRUCKTOR_CHECK
+
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
+#ifdef STREAMS
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(3, 4);
+	//A.print();
+	//B.print();
+	/*Point C = A + B;
+	C.print();
+	A.print();
+	B.print();*/
+	/*A += B;
+	A.print();	*/
+	Point D = A++;
+	A.print();
+	D.print();
+
+	//cout << A << tab << B << endl;
+	//cout << "Введите координаты точки: "; cin >> A;
+	//cout << "Вы ввели : " << A << endl;
+
+	//(ostream)-cout  (Point)A
+	//(istream)-cin  
+#endif // STREAMS
+
+	Point A(2, 3);
+	Point B(3, 4);	
+	cout << (A == B) << endl;
+	//cout << (A != B) << endl;
+
+	cout << A << endl;
+	/*A.set_x(33);
+	A.set_y(44);*/
+
+	A(33, 44);	
+	cout << A << endl;
 }
---
+++ Intro/Point_distance/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+#define tab '\t'
+
+class Point
+{
+public:
+	double x;
+	double y;
+	double distance_to(const Point& B)
+	{
+		return sqrt((pow((this->x - B.x), 2) + pow((this->y - B.y), 2)));
+	}
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Точка создана " << this << endl;
+	}
+	~Point()
+	{
+		cout << this << tab << "Точка уничтожена" << endl;
+	}	
+	void print()const
+	{
+		cout << this << tab << "X = " << x << tab << "Y = " << y << endl << endl;
+	}
+};
+double distance_fo(const Point& A, const Point& B);
+
+int main()
+{
+	setlocale(0, "rus");
+	
+	Point A(0, 10);
+	Point B(10, 10);
+	Point C;
+
+	A.print();
+	B.print();
+	cout << "Растояние между точками : " << distance_fo(A, B) << endl;
+	cout << endl << "---------------------------------" << endl;
+	cout << "Растояние до точки : " << C.distance_to(B) << endl<<endl;
+	return 0;
+}
+
+double distance_fo(const Point& A, const Point& B)
+{
+	return sqrt((pow((A.x - B.x), 2) + pow((A.y - B.y), 2)));
+}
+
+
